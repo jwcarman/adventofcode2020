3ed28cd5a08ba0168e03a77bc32018f76afbfbd4
James Carman <jcarman@callibrity.com>
2020-12-06 11:53:53 -0500

    Adding Copyright.

--- src/test/java/adventofcode/Day6.java
+++ src/test/java/adventofcode/Day6.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2020 James Carman
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package adventofcode;
 
 import java.util.HashSet;
@@ -32,6 +48,7 @@ public class Day6 {
             a
                         
             b""";
+
     @Test
     void part1() {
         final Set<Integer> answers = new HashSet<>();
@@ -54,6 +71,7 @@ public class Day6 {
                 .filter(i -> answers.count(i) == groupSize)
                 .count();
     }
+
     @Test
     void part2() {
         Multiset<Integer> answers = HashMultiset.create(26);
