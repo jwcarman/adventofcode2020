d4866744d96e7b4949c6a8a45cdf22d88c578c6b
James Carman <jcarman@callibrity.com>
2020-12-07 21:35:10 -0500

    Using Commons Math for combinations.

--- src/test/java/adventofcode/Day1Test.java
+++ src/test/java/adventofcode/Day1Test.java
@@ -16,18 +16,21 @@
 
 package adventofcode;
 
-import java.io.StringReader;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.function.Function;
-import java.util.function.Predicate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import adventofcode.io.Input;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.math3.util.CombinatoricsUtils;
 import org.junit.jupiter.api.Test;
 
 import static adventofcode.io.Input.readResource;
-import static com.google.common.collect.Sets.combinations;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -44,47 +47,47 @@ public class Day1Test {
             675
             1456""";
 
+
     @Test
     void part1() {
-        final Set<Integer> numbers = new HashSet<>(Input.readLines(new StringReader(INPUT), Integer::parseInt));
-        final int sum = findSum2020AndMultiply(combinations(numbers, 2));
-        log.info("Part One: {}", sum);
+        log.info("Part One: {}", findEntriesAndMultiply(INPUT, 2));
     }
 
     @Test
     void part2() {
-        final Set<Integer> numbers = new HashSet<>(Input.readLines(new StringReader(INPUT), Integer::parseInt));
-        final int sum = findSum2020AndMultiply(combinations(numbers, 3));
-        log.info("Part Two: {}", sum);
+        log.info("Part Two: {}", findEntriesAndMultiply(INPUT, 3));
     }
 
     @Test
     void example1() {
-        final Set<Integer> numbers = new HashSet<>(Input.readLines(new StringReader(EXAMPLE_INPUT), Integer::parseInt));
-        final int sum = findSum2020AndMultiply(combinations(numbers, 2));
-        assertThat(sum).isEqualTo(514579);
+        assertThat(findEntriesAndMultiply(EXAMPLE_INPUT, 2)).isEqualTo(514579);
     }
 
     @Test
     void example2() {
-        final Set<Integer> numbers = new HashSet<>(Input.readLines(new StringReader(EXAMPLE_INPUT), Integer::parseInt));
-        final int sum = findSum2020AndMultiply(combinations(numbers, 3));
-        assertThat(sum).isEqualTo(241861950);
+        assertThat(findEntriesAndMultiply(EXAMPLE_INPUT, 3)).isEqualTo(241861950);
     }
 
-    private int findSum2020AndMultiply(Set<Set<Integer>> combinations) {
-        return combinations.stream()
-                .filter(sumTo2020())
+    private <T> Stream<List<T>> combinationsOf(List<T> list, int k) {
+        final Spliterator<int[]> spliterator = Spliterators.spliteratorUnknownSize(CombinatoricsUtils.combinationsIterator(list.size(), k), Spliterator.ORDERED);
+        return StreamSupport.stream(spliterator, false)
+                .map(arr -> Arrays.stream(arr).mapToObj(list::get).collect(Collectors.toList()));
+    }
+
+    private long findEntriesAndMultiply(String input, int k) {
+        final List<Integer> numbers = new ArrayList<>(Input.readLines(input, Integer::parseInt));
+        return combinationsOf(numbers, k)
+                .filter(this::sumsUpTo2020)
+                .map(this::multiply)
                 .findFirst()
-                .map(multiply())
-                .orElse(-1);
+                .orElse(-1L);
     }
 
-    private Function<Set<Integer>, Integer> multiply() {
-        return c -> c.stream().reduce(1, (a, b) -> a * b);
+    private long multiply(List<Integer> combo) {
+        return combo.stream().reduce(1, (a, b) -> a * b);
     }
 
-    private Predicate<Set<Integer>> sumTo2020() {
-        return c -> c.stream().mapToInt(i -> i).sum() == 2020;
+    private boolean sumsUpTo2020(List<Integer> combo) {
+        return combo.stream().mapToInt(i -> i).sum() == 2020;
     }
 }
